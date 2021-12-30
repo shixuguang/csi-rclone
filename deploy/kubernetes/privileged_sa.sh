84949055936348d516f07337a023aaf36fb2cf7b
Simon Shi <simon.shi@ibm.com>
2021-12-30 12:00:22 -0500

    use minio as default namespace

--- deploy/kubernetes/privileged_sa.sh
+++ deploy/kubernetes/privileged_sa.sh
@@ -1,2 +1,2 @@
-oc adm policy add-scc-to-user privileged system:serviceaccount:minio:csi-controller-rclone
+#oc adm policy add-scc-to-user privileged system:serviceaccount:minio:csi-controller-rclone
 oc adm policy add-scc-to-user privileged system:serviceaccount:minio:csi-nodeplugin-rclone
